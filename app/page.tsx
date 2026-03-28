ba2a4568d6027e85aef859953fb460631ca7a40b
Jake Johnson <jake@precision.co>
2026-03-27 21:11:02 -0400

    Hello World

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'system-ui',
+      fontSize: '3rem',
+      background: '#000',
+      color: '#fff'
+    }}>
+      <h1>Hello World 🌍</h1>
+    </div>
+  )
+}
